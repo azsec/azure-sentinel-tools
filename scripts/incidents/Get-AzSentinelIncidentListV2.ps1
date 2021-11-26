96dc253e2a0cf1af5750349e58ad70f337ce5ed6
AzSec <azsecblog@gmail.com>
2021-11-26 08:14:22 +0000

    Updated to use latest API

---
+++ scripts/incidents/Get-AzSentinelIncidentListV2.ps1
@@ -0,0 +1,151 @@
+<#
+    .SYNOPSIS
+        This script is used to extract all Azure Sentinel incidents using a new Azure Sentinel Incident API 2020-01-01
+    .DESCRIPTION
+        The script is used to extract all Azure Sentinel incidents.The script extracts the following info:
+            - Incident ID: this is very important because it shall be used when you need to get specific incident. Name can be similar but this ID is unique.
+            - Title: name of that incident
+            - Incident number: it is incremental number when an incident is created.
+            - Incident severity: severity of an incident (Low, Medium, High, Critical)
+            - Status: status of an incident (New, In Progress, Closed)
+            - Classification: it is a classification (False Positive, True Positive)
+            - Classification Comment: it is formerly Close Reason.
+            - Incident label: it is like a tag.
+            - Close Reason: a close status (False Positive, True Positive)
+            - Owner email: email of the assignee. This field is retrieved from AAD user profile.
+            - Assigned To: name of the person who is assigned to work on an incident
+            - Time Generated: alert time generated, incident time generated
+            - Total comment: it is the total number of comments in each incident.
+    .NOTES
+        This script is written with Azure PowerShell (Az) module.
+
+        File Name     : Get-AzSentinelIncidentListV2.ps1
+        Version       : 1.0.0.5
+        Author        : AzSec (https://azsec.azurewebsites.net/)
+        Prerequisite  : Az
+        Reference     : https://azsec.azurewebsites.net/2020/03/18/quick-look-at-new-azure-sentinel-incident-api/
+    .EXAMPLE
+        .\Get-AzSentinelIncidentListV2.ps1 -WorkspaceRg azsec-corporate-rg `
+                                         -WorkspaceName azsec-shared-workspace `
+                                         -FileName 'AzSentinelIncident' `
+                                         -Path 'C:\Audit' 
+#>
+
+Param(
+    [Parameter(Mandatory = $true,
+        HelpMessage = "Resource group name of the Log Analytics workspace Azure Sentinel connects to",
+        Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $WorkspaceRg,
+
+    [Parameter(Mandatory = $true,
+        HelpMessage = "Name of the Log Analytics workspace Azure Sentinel connects to",
+        Position = 1)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $WorkspaceName,
+
+    [Parameter(Mandatory = $true,
+              HelpMessage = "File name of the audit report",
+              Position = 2)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $FileName,
+
+    [Parameter(Mandatory = $true,
+              HelpMessage = "Location where the audit report is stored",
+              Position = 3)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Path
+)
+
+
+
+$date = Get-Date -UFormat "%Y_%m_%d_%H%M%S"
+
+$workspaceId = (Get-AzOperationalInsightsWorkspace -Name $WorkspaceName `
+                                                   -ResourceGroupName $WorkspaceRg).ResourceId
+if (!$workspaceId) {
+    throw "[!] Workspace cannot be found. Please try again"
+}
+else {
+    Write-Host -ForegroundColor Green "[-] Your Azure Sentinel is connected to workspace: $WorkspaceName"
+}
+
+class azSentinelIncidentCsv{
+    [Object]$IncidentUniqueId
+    [Object]$IncidentTile
+    [Object]$IncidentNumber
+    [Object]$Description
+    [Object]$Severity
+    [Object]$Status
+    [Object]$Label
+    [Object]$Classification
+    [Object]$ClassificationComment
+    [Object]$AssignedTo
+    [Object]$OwnerEmail
+    [Object]$CreatedTimeUTC
+    [Object]$FirstActivityTimeUTC
+    [Object]$LastActivityTimeUTC
+    [Object]$LastModifiedTimeUTC
+    [Object]$FirstActivityTimeGenerated
+    [Object]$LastActivityTimeGenerated
+    [Object]$AlertProductName
+    [Object]$CommentCount
+    [Object]$AlertCount
+}
+
+
+# Get Azure Access Token for https://management.azure.com endpoint
+$accessToken = Get-AzAccessToken -ResourceTypeName "ResourceManager"
+$authHeader = @{
+    'Content-Type'  = 'application/json'
+    'Authorization' = 'Bearer ' + $accessToken.Token
+}
+
+$uri = "https://management.azure.com" + $workspaceId + "/providers/Microsoft.SecurityInsights/incidents/?api-version=2020-01-01"
+
+$response = Invoke-RestMethod -Uri $uri `
+                              -Method GET `
+                              -Headers $authHeader
+$icds = $response.value
+
+while($response.nextLink)
+{
+    $nextLink = $response.nextLink
+    $response = (Invoke-RestMethod -Uri $nextLink -Method GET -Headers $authHeader)
+    $icds += $response.value
+}
+
+$azSentinelIncidentCsvReport = @()
+
+foreach ($icd in $icds) {
+    $icdObj = [azSentinelIncidentCsv]::new()
+    # Incident Unique ID is important if you want to get specific incident by ID
+    $icdObj.IncidentUniqueId = $icd.id.Split('/')[12]
+    $icdObj.IncidentTile = $icd.properties.title
+    $icdObj.IncidentNumber = $icd.properties.incidentNumber
+    $icdObj.Description =  $icd.properties.description
+    $icdObj.Severity = $icd.properties.severity
+    $icdObj.Status = $icd.properties.status
+    $icdObj.Label = $icd.properties.labels.labelName | Out-String
+    $icdObj.Classification = $icd.properties.classification
+    $icdObj.ClassificationComment = $icd.properties.classificationComment
+    $icdObj.AssignedTo = $icd.properties.owner.assignedTo
+    $icdObj.OwnerEmail = $icd.properties.owner.email
+    $icdObj.CreatedTimeUTC = $icd.properties.createdTimeUtc
+    $icdObj.FirstActivityTimeUTC = $icd.properties.firstActivityTimeUTC
+    $icdObj.LastActivityTimeUTC = $icd.properties.lastActivityTimeUTC
+    $icdObj.LastModifiedTimeUTC = $icd.properties.lastModifiedTimeUTC
+    $icdObj.FirstActivityTimeGenerated = $icd.properties.firstActivityTimeGenerated
+    $icdObj.LastActivityTimeGenerated = $icd.properties.lastActivityTimeGenerated
+    $icdObj.AlertProductName = $icd.properties.additionalData.alertProductNames | Out-string
+    $icdObj.CommentCount = $icd.properties.additionalData.commentsCount
+    $icdObj.AlertCount = $icd.properties.additionalData.alertsCount
+    $azSentinelIncidentCsvReport += $icdObj
+}
+
+$azSentinelIncidentCsvReport | Export-Csv -Path "$Path\$($FileName)_$($date).csv" -NoTypeInformation -Encoding UTF8
+Write-Host -ForegroundColor Green "[-] YOUR AZURE SENTINEL INCIDENT REPORT IS IN: " $Path\$($FileName)_$($date).csv
---
+++ scripts/incidents/New-AzSentinelIncident.ps1
@@ -0,0 +1,180 @@
+<#
+    .SYNOPSIS
+        This script is used to create a custom Azure Sentinel incident.
+    .DESCRIPTION
+        This script is used to create a custom Azure Sentinel incident that you may need in order to test your own SOAR solution.
+    .NOTES
+        This script is written with Azure PowerShell (Az) module.
+
+        File Name     : New-AzSentinelIncident.ps1
+        Version       : 1.1.0.0
+        Author        : AzSec (https://azsec.azurewebsites.net/)
+        Prerequisite  : Az
+        Reference     : https://azsec.azurewebsites.net/2020/01/06/create-a-fully-customized-azure-sentinel-incident/
+    .EXAMPLE
+        .\New-AzSentinelIncident.ps1 -WorkspaceRg azsec-corporate-rg `
+                                     -WorkspaceName azsec-shared-workspace `
+                                     -IncidentTile "New Incident" `
+                                     -IncidentDescription "Test Incident" `
+                                     -IncidentSeverity "Informational" `
+                                     -IncidentStatus "active" `
+                                     -IncidentOwner "5487dc0c-7765-421c-ad9a-60b81e6f0a83" `
+                                     -LabelName "SSH" `
+                                     -FirstActivityTime "2021-11-27T00:00:00Z" `
+                                     -LastActivityTime "2021-11-27T00:00:00Z" `
+#>
+
+Param(
+    [Parameter(Mandatory = $true,
+               HelpMessage = "Resource group name of the Log Analytics workspace Azure Sentinel connects to",
+               Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $WorkspaceRg,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "Name of the Log Analytics workspace Azure Sentinel connects to",
+               Position = 1)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $WorkspaceName,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The title of the incident",
+               Position = 2)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $IncidentTile,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The description of the incident",
+               Position = 3)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $IncidentDescription,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The title of the incident",
+               Position = 4)]
+    [ValidateSet("High",
+                 "Medium",
+                 "Low",
+                 "Informational")]
+    [string]
+    $IncidentSeverity,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The title of the incident",
+               Position = 5)]
+    [ValidateSet("New",
+                 "Active",
+                 "Closed")]
+    [string]
+    $IncidentStatus,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The owner of the incident",
+               Position = 6)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $IncidentOwnerObjectId,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The label of the incident",
+               Position = 7)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $LabelName,
+
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The date time (UTC) of the first activity of the incident",
+               Position = 8)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $FirstActivityTime,
+
+    [Parameter(Mandatory = $true,
+               HelpMessage = "The date time (UTC) of the last activity of the incident",
+               Position = 9)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $LastActivityTime
+)
+
+$workspaceId = (Get-AzOperationalInsightsWorkspace -Name $WorkspaceName `
+                                                   -ResourceGroupName $WorkspaceRg).ResourceId
+if (!$workspaceId) {
+    throw "[!] Workspace cannot be found. Please try again"
+}
+else {
+    Write-Host -ForegroundColor Green "[-] Your Azure Sentinel is connected to workspace: $WorkspaceName"
+}
+# Get Azure Access Token for https://management.azure.com endpoint
+$accessToken = Get-AzAccessToken -ResourceTypeName "ResourceManager"
+$authHeader = @{
+    'Content-Type'  = 'application/json'
+    'Authorization' = 'Bearer ' + $accessToken.Token
+}
+
+# The name of Incident is a global unique ID (GUID)
+$incidentName = (New-Guid).Guid
+$incident = [ordered]@{
+    name = $incidentName
+    properties = @{
+        title = $IncidentTile
+        description = $IncidentDescription
+        owner = @{
+            objectId = $IncidentOwner
+        }
+        labels = @(
+            @{
+                labelName = $LabelName
+                labelType = "User"
+            }
+        )
+        status = $IncidentStatus
+        severity = $IncidentSeverity
+        firstActivityTimeUtc = $FirstActivityTime
+        lastActivityTimeUtc = $LastActivityTime
+    }
+}
+
+# Sample Request Body
+##########################################################
+# {
+#     "name": "e5f4ec2c-53ec-4fe8-9590-64bc0a78dd97",
+#     "properties": {
+#         "owner": {
+#             "objectId": null
+#         },
+#         "labels": [
+#             {
+#                 "labelName": "SSH",
+#                 "labelType": "User"
+#             }
+#         ],
+#         "title": "New Incident",
+#         "description": "Test Incident",
+#         "status": "active",
+#         "severity": "Informational",
+#         "firstActivityTimeUtc": "2021-11-27T00:00:00Z",
+#         "lastActivityTimeUtc": "2021-11-27T00:00:00Z"
+#     }
+# }
+############################################################
+
+$requestBody = $incident | ConvertTo-Json -Depth 3
+$uri = "https://management.azure.com" + $workspaceId `
+                                      + "/providers/Microsoft.SecurityInsights/incidents/" `
+                                      + $incidentName `
+                                      + "?api-version=2021-04-01"
+
+
+$response = Invoke-RestMethod -Uri $uri `
+                              -Method PUT `
+                              -Headers $authHeader `
+                              -Body $requestBody
+
+$response.properties
+
